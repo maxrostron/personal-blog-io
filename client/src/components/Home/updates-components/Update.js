b904afdfd214fcfaf84ca6e0416b616b85259456
maxrostron <maxrostron@hotmail.co.uk>
2021-08-25 15:19:53 +0100

    Build Home Update foundations

---
+++ client/src/components/Home/updates-components/Update.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Update() {
+  return (
+    <div className="home__update">
+      <div className="home__update-color"></div>
+      <div className="home__update-content">
+        <div className="home__update-data">
+          <h3 className="home__update-path">blog//opinions//</h3>
+          <h3 className="home__update-date">25th August 2021</h3>
+          <div className="home__update-new">NEW</div>
+        </div>
+        <h1 className="home__update-title">Lorem ipsum dolor sit amet</h1>
+        <h2 className="home__update-blurb">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod
+          tempor.{" "}
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Update;
--- client/src/components/Home/updates-components/UpdateBox.js
+++ client/src/components/Home/updates-components/UpdateBox.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Update from "./Update";
 
 function UpdateBox() {
-  return <div className="home__update-container"></div>;
+  return (
+    <div className="home__update-container">
+      <h1>Recent Updates:</h1>
+
+      <Update />
+      <Update />
+      <Update />
+      <Update />
+      <Update />
+      <Update />
+    </div>
+  );
 }
 
 export default UpdateBox;
