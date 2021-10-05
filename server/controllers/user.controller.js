498fa687de661c61890c4571953af90cc51df057
maxrostron <maxrostron@hotmail.co.uk>
2021-10-05 17:28:45 +0100

    Fix nodemailer bug

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import crypto from "crypto";
-import nodemailer from "nodemailer";
+import { createTransport } from "nodemailer";
 import sendgridTransport from "nodemailer-sendgrid-transport";
 import User from "../models/userModels/user.js";
 import Token from "../models/userModels/token.js";
@@ -36,7 +36,7 @@ export const signup = function (req, res, next) {
           }
 
           // Send email (use verified sender's email address & generated API_KEY on SendGrid)
-          const transporter = nodemailer.createTransport(
+          const transporter = createTransport(
             sendgridTransport({
               auth: {
                 api_key: process.env.SENDGRID_APIKEY,
@@ -154,7 +154,7 @@ export const resendLink = function (req, res, next) {
         }
 
         // Send email (use verified sender's email address & generated API_KEY on SendGrid)
-        const transporter = nodemailer.createTransport(
+        const transporter = createTransport(
           sendgridTransport({
             auth: {
               api_key: process.env.SENDGRID_APIKEY,
